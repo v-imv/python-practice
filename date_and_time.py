444cbf6e4ae925331de8d2fdcb025f87afcaf24e
v-imv <v.iminov@gmail.com>
2022-09-23 12:39:37 +0100

    working with dates and times

---
+++ date_and_time.py
@@ -0,0 +1,7 @@
+# To get current date and time must use datetime library
+from datetime import datetime
+
+current_date = datetime.now()
+print(current_date)
+
+print("Today is: " + str(current_date))
