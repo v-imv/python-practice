486427488a283566a22234e355dfc0f0cc6e9f2e
v-imv <v.iminov@gmail.com>
2022-09-23 12:44:07 +0100

    specifying date information

--- date_and_time.py
+++ date_and_time.py
@@ -8,6 +8,7 @@ print(current_date)
 print("Today is: " + str(current_date))
 """
 
+"""
 from datetime import datetime, timedelta
 today = datetime.now()
 print("Today is: " + str(today))
@@ -15,3 +16,11 @@ print("Today is: " + str(today))
 one_day = timedelta(days=1)
 yesterday = today - one_day
 print("Yesterday was " + str(yesterday))
+"""
+
+from datetime import datetime
+current_date = datetime.now()
+
+print("Day: " + str(current_date.day))
+print("Month: " + str(current_date.month))
+print("Year: " + str(current_date.year))
