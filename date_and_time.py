a639b1f1fec5d1b26dceaf5aa6601e2364c75c14
v-imv <v.iminov@gmail.com>
2022-09-28 23:12:11 +0100

    commenting date and time practice

--- date_and_time.py
+++ date_and_time.py
@@ -1,29 +1,25 @@
-"""
 # To get current date and time must use datetime library
-from datetime import datetime
+# from datetime import datetime
 
-current_date = datetime.now()
-print(current_date)
+# current_date = datetime.now()
+# print(current_date)
+# print("Today is: " + str(current_date))
 
-print("Today is: " + str(current_date))
-"""
 
-"""
-from datetime import datetime, timedelta
-today = datetime.now()
-print("Today is: " + str(today))
+# Using time delta to figure out what day it was yesterday
+# from datetime import datetime, timedelta
+# today = datetime.now()
+# print("Today is: " + str(today))
 
-one_day = timedelta(days=1)
-yesterday = today - one_day
-print("Yesterday was " + str(yesterday))
-"""
+# one_day = timedelta(days=1)
+# yesterday = today - one_day
+# print("Yesterday was " + str(yesterday))
 
-"""
+
+# Printing only the relevant information that I need
 from datetime import datetime
 current_date = datetime.now()
 
 print("Day: " + str(current_date.day))
 print("Month: " + str(current_date.month))
 print("Year: " + str(current_date.year))
-"""
-
