627a3710bc58365a5883fee482573620ee449f1f
v-imv <v.iminov@gmail.com>
2022-09-29 21:26:46 +0100

    python lists

---
+++ collections.py
@@ -0,0 +1,9 @@
+# Lists are ordered, changeable and allow duplicate values
+names = ["Harry", "Daniel", "Sarah", "Keyma"]
+print(names)
+
+scores = []
+scores.append(80)
+scores.append(91)
+print(scores)
+print(names[2])
