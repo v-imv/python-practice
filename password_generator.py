e86273178bb48be6a53bbe0c85b2fa28200eda06
v-imv <v.iminov@gmail.com>
2022-09-29 22:13:13 +0100

    random password generator

---
+++ password_generator.py
@@ -0,0 +1,14 @@
+from operator import countOf
+import random
+
+usable_characters = ("aAbBcCdDeEfFgGhHjJkKlLmMnNoOpPqQrRsStTuUvV1234567890-=+_)(*&^%$£!'")
+max_index = len(usable_characters) - 1
+password = ""
+
+x = 0
+max_pw_characters = 20
+while x < max_pw_characters:
+    rand = random.randrange(0,max_index)
+    password = password + usable_characters[rand]
+    x = x + 1
+print("Here is your new 20 character password: " + str(password))
